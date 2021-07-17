823de998c1c60273784813867b83e9b87c9dfa89
awvalent11 <awvalent11@gmail.com>
2021-07-17 10:52:58 -0500

    Add files via upload
    React file with added 'react' and 'react-dom' imports

--- reactCONOP.js
+++ reactCONOP.js
@@ -1,5 +1,19 @@
-import ScriptTag from 'react-script-tag';
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
 const Demo = props => (
 <ScriptTag type="text/javascript" src="/path/to/resource.js" />
 )
 
+const obj1 = (
+    <h1>This is the "React" heading</h1>
+)
+ReactDOM.render(obj1, document.getElementById('heading'));
+
+
+//React.createElement({draw}? {draw}: set display to none)
+
+CONOPClass extends React.Component() {
+    render 
+}
